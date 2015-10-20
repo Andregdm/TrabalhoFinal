73c207bcb214598b67ffa0fd1815b7546809f5b5
Andregdm <andreg.dmenezes@gmail.com>
2015-10-20 11:07:03 -0200

    Basic for Play and Menu classes

--- src/trabalhofinal/Jogo.java
+++ src/trabalhofinal/Jogo.java
@@ -5,17 +5,49 @@
  */
 package trabalhofinal;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
 /**
  *
  * @author AGM
  */
-public class Jogo {
+public class Jogo extends StateBasedGame{
+
+    public static final String nomeDoJogo = "Race Obstacles!";
+    public static final int menu = 0;
+    public static final int play = 1;
+
+    public Jogo(String gName) {
+        super(gName);
+        this.addState(new Menu(menu));
+        this.addState(new Play(play));
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        AppGameContainer appgc;
+        try {
+            appgc = new AppGameContainer(new Jogo(nomeDoJogo));
+            appgc.setDisplayMode(640, 360, false);
+            appgc.start();
+        } catch (SlickException ex) {
+            Logger.getLogger(Jogo.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        }
     }
-    
+
+    @Override
+    public void initStatesList(GameContainer gc) throws SlickException {
+        this.getState(menu).init(gc, this);
+        this.getState(play).init(gc, this);
+        this.enterState(menu);
+    }
+
 }
