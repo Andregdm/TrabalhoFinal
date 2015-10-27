954d314719b4cab1c02111e6bac57c219cc099e9
Andregdm <andreg.dmenezes@gmail.com>
2015-10-27 14:52:17 -0200

    Tela Preta Funcionando
    Mistura do projeto com a base do projeto do Dutra, agora é possível
    entrar na tela preta do tutorial.

--- src/trabalhofinal/Jogo.java
+++ src/trabalhofinal/Jogo.java
@@ -7,10 +7,8 @@ package trabalhofinal;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.SlickException;
-import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
 
 /**
  *
